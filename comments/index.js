a6224cb8b20a0445ecacab8a85f4dec2a7b531d4
Nari <narimanethr@gmail.com>
2024-12-19 17:24:47 +0100

    debugage pour les commentaires

--- comments/index.js
+++ comments/index.js
@@ -10,6 +10,12 @@ app.use(cors());
 
 const commentsByPostId = {};
 
+app.use((req, res, next) => {
+  console.log("Request received", req.method, req.url);
+  next();
+}  
+)
+
 app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
--- event-bus/index.js
+++ event-bus/index.js
@@ -7,6 +7,12 @@ app.use(bodyParser.json());
 
 const events = [];
 
+app.use((req, res, next) => {
+  console.log("Request received", req.method, req.url);
+  next();
+}  
+)
+
 app.post("/events", (req, res) => {
   const event = req.body;
 
